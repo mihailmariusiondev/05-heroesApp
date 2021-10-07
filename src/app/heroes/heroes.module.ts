1c3991c63121503827bf390b5adfe6d9f7ecb91f
Marius Mihail Ion <90253567+mihailmariusiondev@users.noreply.github.com>
2021-10-07 11:18:27 +0200

    angular flex layout, add material components to material module, add sidenav

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -6,6 +6,8 @@ import { HomeComponent } from './pages/home/home.component';
 import { ListadoComponent } from './pages/listado/listado.component';
 import { BuscarComponent } from './pages/buscar/buscar.component';
 import { HeroesRoutingModule } from './heroes-routing.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MaterialModule } from '../material/material.module';
 
 
 
@@ -19,7 +21,9 @@ import { HeroesRoutingModule } from './heroes-routing.module';
   ],
   imports: [
     CommonModule,
-    HeroesRoutingModule
+    HeroesRoutingModule,
+    FlexLayoutModule,
+    MaterialModule
   ]
 })
 export class HeroesModule { }
