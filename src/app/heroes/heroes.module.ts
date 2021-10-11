dfd92734f5194864e04d193fe87e1569d0089142
Marius Mihail Ion <90253567+mihailmariusiondev@users.noreply.github.com>
2021-10-11 13:54:37 +0200

    material autocomplete

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -11,6 +11,7 @@ import { FlexLayoutModule } from '@angular/flex-layout';
 import { HeroeTarjetaComponent } from './components/heroe-tarjeta/heroe-tarjeta.component';
 import { ImagenPipe } from './pipes/imagen.pipe';
 import { EditarComponent } from './pages/editar/editar/editar.component';
+import { FormsModule } from '@angular/forms';
 
 
 
@@ -29,7 +30,8 @@ import { EditarComponent } from './pages/editar/editar/editar.component';
     CommonModule,
     HeroesRoutingModule,
     MaterialModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    FormsModule
   ]
 })
 export class HeroesModule { }
