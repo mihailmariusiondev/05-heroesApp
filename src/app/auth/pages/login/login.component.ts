f6bf4d283e45e6a96244ed6b5cfd0642f5ad5585
Marius Mihail Ion <90253567+mihailmariusiondev@users.noreply.github.com>
2021-10-15 09:46:00 +0200

    auth service authentication status

--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
     selector: 'app-login',
@@ -9,13 +10,25 @@ import { Router } from '@angular/router';
 })
 export class LoginComponent {
 
-    constructor(private router: Router) { }
+    constructor(
+        private router: Router,
+        private authService: AuthService
+    ) { }
 
     login() {
         // Ir al backend
         // Un usuario
 
-        this.router.navigate(['/heroes'])
+        this.authService.login()
+            .subscribe(resp => {
+                console.log(resp);
+
+                if (resp.id) {
+                    this.router.navigate(['/heroes'])
+                }
+            })
+
+
     }
 
 }
